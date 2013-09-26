834535f71b5e64152fc9730b285005d467f5f834
Gyuri Horak <gyorgy.horak@euedge.com>
2013-09-26 18:08:15 +0200

    my layout, except function keys

--- keyboard/ergodox/keymap_repa.h
+++ keyboard/ergodox/keymap_repa.h
@@ -1,6 +1,6 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * Keymap: Default Layer in QWERTY
+     * Keymap: Default Layer in DVORAK
      *
      * ,--------------------------------------------------.           ,--------------------------------------------------.
      * | Esc    |   1  |   2  |   3  |   4  |   5  | +L2  |           | +L1  |   6  |   7  |   8  |   9  |   0  |   =    |
@@ -26,28 +26,28 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      ****************************************************************************************************
      */
 
-    KEYMAP(  // Layer0: default, leftled:none
+    KEYMAP(  // Layer0: dvorak
         // left hand
-        ESC, 1,   2,   3,   4,   5,    FN3,
-        TAB, QUOT,COMM,DOT, P,   Y,    FN6,
+        ESC, 1,   2,   3,   4,   5,    FN4,
+        TAB, QUOT,COMM,DOT, P,   Y,    FN1,
         RALT,A,   O,   E,   U,   I,
-        LSFT,SCLN,Q,   J,   K,   X,    FN2,
+        LSFT,SCLN,Q,   J,   K,   X,    FN3,
         LCTL,GRV, BSLS,LBRC,RBRC,
                                       LGUI,LALT,
-                                            FN2,
-                                 SPC,BSPC,  FN5,
+                                            FN3,
+                                 SPC,BSPC,  FN6,
         // right hand
-              FN1,6,   7,   8,   9,   0,   EQL,
-              FN0,F,   G,   C,   R,   L,   SLSH,
+              FN2,6,   7,   8,   9,   0,   EQL,
+              FN5,F,   G,   C,   R,   L,   SLSH,
                   D,   H,   T,   N,   S,   MINS,
-              FN2,B,   M,   W,   V,   Z,   RSFT,
+              FN3,B,   M,   W,   V,   Z,   RSFT,
                        LEFT,DOWN,UP,  RGHT,RCTL,
         RALT,RGUI,
         PGUP,
         PGDN,ENT, SPC 
     ),
 
-    KEYMAP(  // Layer1: Workman layout, leftled:all
+    KEYMAP(  // Layer1: function and specal keys TODO
         // left hand
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,Q,   D,   R,   W,   B,   TRNS,
@@ -59,89 +59,39 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  TRNS,TRNS,TRNS,
         // right hand
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-             TRNS,J,   F,   U,   P,   4,   TRNS,
-                  Y,   N,   E,   O,   I,   TRNS,
-             TRNS,K,   L,   TRNS,TRNS,TRNS,TRNS,
+             TRNS,Y,   U,   I,   O,   P,   TRNS,
+                  H,   J,   K,   L,   SCLN,TRNS,
+             TRNS,N,   M,   COMM,DOT, QUOT,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // Layer2: numpad, leftled:blue
+    KEYMAP(  // Layer2: qwerty
         // left hand
-        TRNS,NO,  NO,  NO,  NO,  PAUS,PSCR,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  NO,  TRNS,NO,
-        TRNS,NO,  NO,  NO,  TRNS,NO,  TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,Q,   W,   E,   R,   T,   TRNS,
+        TRNS,A,   S,   D,   F,   G,
+        TRNS,Z,   X,   C,   V,   B,   TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             SLCK,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
-                  TRNS,P4,  P5,  P6,  PPLS,PENT,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
-                       P0,  PDOT,SLSH,PENT,PENT,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // Layer3: F-keys + PgUp/PgDn on right hand, leftled:green
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  TRNS,NO,  NO,  NO,
-        TRNS,NO,  TRNS,NO,  NO,  NO,  TRNS,
-        TRNS,TRNS,TRNS,LALT,LGUI,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 LCTL,LSFT,TRNS,
-        // right hand
-             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,NO,  F1,  F2,  F3,  F4,  PGUP,
-                  NO,  F5,  F6,  F7,  F8,  PGDN,
-             TRNS,NO,  F9,  F10, F11, F12, NO,
-                       RGUI,RALT,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,RSFT,RCTL
-    ),
-
-    KEYMAP(  // Layer4: unconvenient keys on right hand, leftled:red
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,TRNS,NO,  NO,  NO,  NO,
-        TRNS,TRNS,NO,  NO,  NO,  NO,  TRNS,
-        TRNS,TRNS,TRNS,LALT,LGUI,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-
-        // in Workman right hand will be:
-        //              =
-        //    ^ { } ( ) +
-        //    ' ! $ " ; \
-        //    # [ < > ] AppMenu
-        //
-
-        // right hand
-             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,MINS,4,   5,   9,   0,   PPLS,
-                  BSLS,2,   P,   FN1, 1,   FN2,
-             TRNS,3,   6,   FN3, FN4, 7,   APP,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,Y,   U,   I,   O,   P,   TRNS,
+                  H,   J,   K,   L,   SCLN,TRNS,
+             TRNS,N,   M,   COMM,DOT, QUOT,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // Layer5: F-keys instead of numbers, leftled:red
+    KEYMAP(  // Layer3: keyboard functions
         // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  F6,
+        FN0, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -150,7 +100,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             F7,  F8,  F9,  F10, F11, F12, TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
                   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -160,69 +110,22 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // Layer6: F-keys, cursor, Workman-layer switch, Teensy, leftled:red+onboard
+    KEYMAP(  // Layer4: numpad, mirrored dvorak for left hand
         // left hand
-        TRNS,F1,  F2,  F3,  F4,  F5,  F6,
-        FN0, NO,  PGUP,UP,  PGDN,PGUP,TRNS,
-        TRNS,NO,  LEFT,DOWN,RGHT,PGDN,
-        TRNS,INS, DEL, END, HOME,NO,  TRNS,
-        FN16,TRNS,TRNS,TRNS,TRNS,
+        EQL, 0,   9,   8,   7,   6,   TRNS,
+        SLSH,L,   R,   C,   G,   F,   TRNS,
+        MINS,S,   N,   T,   H,   D, 
+        RSFT,Z,   V,   W,   M,   B,   TRNS,
+        RCTL,LEFT,DOWN,UP,  RGHT,
                                       TRNS,TRNS,
                                            TRNS,
-                                 TRNS,TRNS,TRNS,
+                                 TRNS,ENT, TRNS,
         // right hand
-             F7,  F8,  F9,  F10, F11, F12, MINS,
-             TRNS,PGUP,PGUP,UP,  PGDN,NO,  FN0,
-                  PGDN,LEFT,DOWN,RGHT,NO,  TRNS,
-             TRNS,NO,  HOME,END, DEL, INS, TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    //
-    // rarely used
-    //
-
-    KEYMAP(  // Layer7: F-keys only, leftled:red
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,F13, F14, F15, F16, NO,  TRNS,
-        TRNS,F17, F18, F19, F20, NO,
-        TRNS,F21, F22, F23, F24, NO,  TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-        // right hand
-             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,NO,  F1,  F2,  F3,  F4,  TRNS,
-                  NO,  F5,  F6,  F7,  F8,  TRNS,
-             TRNS,NO,  F9,  F10, F11, F12, TRNS,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,
-        TRNS,
-        TRNS,TRNS,TRNS
-    ),
-
-    KEYMAP(  // Layer8: mouse and navigation, leftled:blue and green
-        // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  TRNS,NO,  NO,
-        TRNS,NO,  NO,  TRNS,NO,  NO,  TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                 TRNS,TRNS,TRNS,
-
-        // right hand
-             TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-             TRNS,BTN2,WH_L,WH_U,WH_D,WH_R,PGUP,
-                  BTN1,MS_L,MS_U,MS_D,MS_R,PGDN,
-             TRNS,BTN3,HOME,END, DEL, INS, NO,
-                       TRNS,TRNS,TRNS,TRNS,TRNS,
+             SLCK,NLCK,PSLS,PAST,PAST,PMNS,BSPC,
+             TRNS,NO,  P7,  P8,  P9,  PMNS,BSPC,
+                  NO,  P4,  P5,  P6,  PPLS,PENT,
+             TRNS,NO,  P1,  P2,  P3,  PENT,TRNS,
+                       P0,  PDOT,SLSH,PENT,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
@@ -286,41 +189,13 @@ enum function_id {
 static const uint16_t PROGMEM fn_actions[] = {
     ACTION_FUNCTION(TEENSY_KEY),                    // FN0  - Teensy key
 
-    ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),             // FN1  = Shifted BackSlash // " in Workman
-    ACTION_MODS_KEY(MOD_LSFT, KC_MINS),             // FN2  = Shifted Minus     // \ in Workman
-    ACTION_MODS_KEY(MOD_LSFT, KC_COMM),             // FN3  = Shifted comma     // < in Workman
-    ACTION_MODS_KEY(MOD_LSFT, KC_DOT),              // FN4  = Shifted dot       // > in Workman
+    ACTION_LAYER_SET(0, ON_BOTH),                   // FN1 - set Layer0, dvorak
+    ACTION_LAYER_SET(1, ON_BOTH),                   // FN2 - set Layer1, function keys
+    ACTION_LAYER_MOMENTARY(1),                      // FN3 - momentary Layer1, function keys
+    ACTION_LAYER_SET(2, ON_BOTH),                   // FN4 - set Layer2, qwerty
+    ACTION_LAYER_MOMENTARY(3),                      // FN5 - set Layer3, bootloader
+    ACTION_LAYER_MOMENTARY(4),                      // FN6 - set Layer4, to use with Numpad keys and mirrored left
 
-    ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSPC),         // FN5  = LShift with tap BackSpace
-    ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ESC),          // FN6  = LCtrl  with tap Escape
-    ACTION_MODS_TAP_KEY(MOD_LALT, KC_SPC),          // FN7  = LAlt   with tap Space
-    ACTION_MODS_TAP_KEY(MOD_RALT, KC_INS),          // FN8  = RAlt   with tap Ins
-    ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT),          // FN9  = RShift with tap Enter
-    ACTION_MODS_TAP_KEY(MOD_RCTL, KC_SPC),          // FN10 = RCtrl  with tap Space
-
-    ACTION_MODS_TAP_KEY(MOD_LSFT, KC_TAB),          // FN11 = LShift with tap Tab
-    ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC),          // FN12 = LGui   with tap Escape
-    ACTION_MODS_TAP_KEY(MOD_RSFT, KC_QUOT),         // FN13 = RShift with tap quotes
-    ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RBRC),         // FN14 = RCtrl  with tap ]
-
-    ACTION_LAYER_SET(0, ON_BOTH),                   // FN15 - set Layer0
-    ACTION_LAYER_SET(1, ON_BOTH),                   // FN16 - set Layer1, to use Workman layout at firmware level
-    ACTION_LAYER_SET(2, ON_BOTH),                   // FN17 - set Layer2, to use with Numpad keys
-
-    ACTION_LAYER_MOMENTARY(2),                      // FN18 - momentary Layer2, to use with Numpad keys
-    ACTION_LAYER_MOMENTARY(5),                      // FN19 - momentary Layer5, to use with F* keys on top row
-    ACTION_LAYER_MOMENTARY(6),                      // FN20 - momentary Layer6, to use with F* keys on top row, cursor, Teensy, Workman-layer switch
-    ACTION_LAYER_MOMENTARY(7),                      // FN21 - momentary Layer7, to use with F* keys (F1-F24)
-
-    ACTION_LAYER_TAP_KEY(2, KC_F),                  // FN22 = momentary Layer2 on F key, to use with Numpad keys
-    ACTION_LAYER_TAP_KEY(3, KC_S),                  // FN23 = momentary Layer3 on S key, to use with F* keys
-    ACTION_LAYER_TAP_KEY(4, KC_A),                  // FN24 = momentary Layer4 on A key, to use with unconvenient keys
-    ACTION_LAYER_TAP_KEY(8, KC_D),                  // FN25 = momentary Layer8 on D key, to use with mouse and navigation keys
-
-    ACTION_LAYER_TAP_KEY(2, KC_V),                  // FN26 = momentary Layer2 on V key, to use with Numpad keys
-    ACTION_LAYER_TAP_KEY(3, KC_X),                  // FN27 = momentary Layer3 on X key, to use with F* keys
-    ACTION_LAYER_TAP_KEY(4, KC_Z),                  // FN28 = momentary Layer4 on Z key, to use with unconvenient keys
-    ACTION_LAYER_TAP_KEY(8, KC_C),                  // FN29 = momentary Layer8 on C key, to use with mouse and navigation keys
 };
 
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
